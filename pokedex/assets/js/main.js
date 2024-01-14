14bc9ba1ce1be29d99e066d00f9c7994d6e7c7b7
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-01-14 02:22:55 -0300

    feat: list types

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -3,6 +3,23 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">Bulbasaur</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>                    
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="Bulbasaur">
+        </div>                
+    </li>
+    `
+}
+
 // Usando Promisses
 fetch(url)
     .then((response) => response.json())
@@ -12,5 +29,4 @@ fetch(url)
         console.log(pokemonList)
     })
     .catch((error) => console.error(error))
-
 
