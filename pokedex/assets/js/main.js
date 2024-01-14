07690c3c878fc66f8c1b35cdc81be23bb8a31cb5
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-01-14 03:19:10 -0300

    refactor: refactoring code

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,6 @@
 
 const offset = 0
-const limit = 10
+const limit = 0
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
@@ -22,15 +22,11 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-// Usando Promisses
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i]
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)         
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i]
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+})
+
 
