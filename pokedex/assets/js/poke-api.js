8327cd05a98df317ab8f7169cbdda636713158ad
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-01-15 21:48:30 -0300

    refactor: refactoring code

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -3,13 +3,22 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.name = pokeDetail.name
     pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    pokemon.abilities = pokeDetail.abilities
+    pokemon.hitpoints = pokeDetail.hitpoints
+    pokemon.attack = pokeDetail.attack
+    pokemon.defense = pokeDetail.defense
+
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+
     pokemon.types = types
     pokemon.type = type
+
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
     return pokemon
 }
 
@@ -21,10 +30,11 @@ pokeApi.getPokemonDetail = (pokemon) => {
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)   
+        .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-    }
+}
