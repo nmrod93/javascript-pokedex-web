8ac6e4f6aeba812304e88f6c0328119737a9d560
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-01-19 22:12:17 -0300

    feat: adjusting modal responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,57 +47,54 @@ loadMoreButton.addEventListener('click', () => {
 
 function handleOpenModal(pokemon) {
     // console.log(pokemon)
-    var modal = document.getElementById('modal')
+    // var modal = document.getElementById('modal')
     var modalContent = document.querySelector('.modal-content')
 
     var modalHTML = `
-            <div class="modal-content">
-                <div>
-                    <span class="close" onclick="handleCloseModal()">&times;</span>
+            <div>
+                <span class="close" onclick="handleCloseModal()">&times;</span>
+            </div>
+            <div class="modalDetail ${pokemon.type}">
+                <span class="pokeName">${pokemon.name}</span>
+                <span class="pokeNumber">#${pokemon.number}</span>
+                <span class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </span>
+            <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+            </div>                
+            <div class="stats">Base Stats</div>  
+            <div class="poke-detail">   
+                <div class="stats-detail">Attack: 
+                    <span class="atk">${pokemon.attack}</span>
                 </div>
-                <div class="modalDetail ${pokemon.type}">
-                    <span class="pokeName">${pokemon.name}</span>
-                    <span class="pokeNumber">#${pokemon.number}</span>
-                    <span class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </span>
-                <img src="${pokemon.photo}"
-                    alt="${pokemon.name}">
-                </div>                
-                <div class="stats">Base Stats</div>  
-                <div class="poke-detail">   
-                    <div class="atkDetail">Attack: 
-                        <span class="atk">${pokemon.attack}</span>
-                    </div>
-                    <div class="defDetail">Defense: 
-                        <span class="def">${pokemon.defense}</span>
-                    </div>
-                    <div class="hpDetail">Hitpoints: 
-                        <span class="hp">${pokemon.hp}</span>
-                    </div>
-                    <div class="spcAtkDetail">Special Attack: 
-                        <span class="spcAtk">${pokemon.specialAttack}</span>
-                    </div>
-                    <div class="spcDefDetail">Special Defense: 
-                        <span class="spcDef">${pokemon.specialDefense}</span>
-                    </div>
-                    <div class="spdDetail">Speed: 
-                        <span class="spd">${pokemon.speed}</span>
-                    </div>
+                <div class="stats-detail">Defense: 
+                    <span class="def">${pokemon.defense}</span>
                 </div>
-                <div class="about">About</div>
-                <div class="aboutDetail">
-                    <div class="heightDetail">Height: 
-                        <span class="height">${pokemon.height}
-                    </div>
-                    <div class="weightDetail">Weight: 
-                        <span class="weight">${pokemon.weight}
-                    </div>
+                <div class="stats-detail">Hitpoints: 
+                    <span class="hp">${pokemon.hp}</span>
+                </div>
+                <div class="stats-detail">Special Attack: 
+                    <span class="spcAtk">${pokemon.specialAttack}</span>
+                </div>
+                <div class="stats-detail">Special Defense: 
+                    <span class="spcDef">${pokemon.specialDefense}</span>
+                </div>
+                <div class="stats-detail">Speed: 
+                    <span class="spd">${pokemon.speed}</span>
                 </div>
             </div>
-        `
+            <div class="about">About</div>
+            <div class="poke-detail">
+                <div class="stats-detail">Height: 
+                    <span class="height">${pokemon.height}
+                </div>
+                <div class="stats-detail">Weight: 
+                    <span class="weight">${pokemon.weight}
+                </div>
+            </div>
+    `
         
-
     modalContent.innerHTML = modalHTML
     modal.style.display = 'block'   
 }
