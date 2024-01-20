216fb20460a5adee5c3b2d7a46de5fb6d9764d7c
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-01-20 02:27:21 -0300

    feat: finished project for bootcamp

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -12,7 +12,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.abilities = pokeDetail.abilities.map((typeAbilities) => typeAbilities.ability.name)
     pokemon.stats = pokeDetail.stats.map((typeStats) => pokemon[typeStats.stat.name] = typeStats.base_stat)
-    console.log(pokemon)
+    // console.log(pokemon)
     pokemon.specialAttack = pokemon.stats[3]
     pokemon.specialDefense = pokemon.stats[4]
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
